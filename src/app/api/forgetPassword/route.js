f1e830be2dea2f11e07688994e1bf2920fe0b396
kapilchandekar <kapilchandekar13637@gmail.com>
2024-06-16 13:11:09 +0530

    remove f
    Signed-off-by: kapilchandekar <kapilchandekar13637@gmail.com>

--- src/app/api/forgetPassword/route.js
+++ src/app/api/forgetPassword/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import mongoose from "mongoose";f
+import mongoose from "mongoose";
 import { sendEmail } from "@/helper/mailer";
 import { user } from "@/lib/model/user";
 
