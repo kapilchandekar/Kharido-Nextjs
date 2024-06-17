2fdf8a963156741eba2369a31d28710eacbbf232
kapilchandekar <kapilchandekar13637@gmail.com>
2024-06-17 13:12:49 +0530

    update add To cart code
    Signed-off-by: kapilchandekar <kapilchandekar13637@gmail.com>

--- src/lib/productSlice/cartSlice.js
+++ src/lib/productSlice/cartSlice.js
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const getInitialCartItems = () => {
+  if (typeof window !== 'undefined') {
+    const storedCartItems = localStorage.getItem("cartItems");
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  }
+  return [];
+};
+
 let initialState = {
-  cartItems:  JSON.parse(localStorage.getItem("cartItems")) && [],
+  cartItems: getInitialCartItems(),
   shippingAddress: {},
   paymentMethod: "",
 };
@@ -13,23 +21,29 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
       if (state.cartItems.find((i) => i._id === item._id)) {
-        alert("Item Already Exiting in Cart");
+        alert("Item Already Existing in Cart");
       } else {
         state.cartItems = [...state.cartItems, item];
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        if (typeof window !== 'undefined') {
+          localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        }
       }
     },
     removeFromCart: (state, action) => {
       const id = action.payload;
       state.cartItems = state.cartItems.filter((i) => i._id !== id);
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      if (typeof window !== 'undefined') {
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      }
     },
     increaseQuantity: (state, action) => {
       const id = action.payload;
       const item = state.cartItems.find((i) => i._id === id);
       if (item.availableQty < 2) {
         item.availableQty += 1;
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        if (typeof window !== 'undefined') {
+          localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        }
       } else {
         alert("We're sorry! Only 2 unit(s) allowed in each order");
       }
@@ -39,20 +53,29 @@ const cartSlice = createSlice({
       const item = state.cartItems.find((i) => i._id === id);
       if (item && item.availableQty > 1) {
         item.availableQty -= 1;
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        if (typeof window !== 'undefined') {
+          localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        }
       } else if (item && item.availableQty === 1) {
         state.cartItems = state.cartItems.filter((i) => i._id !== id);
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        if (typeof window !== 'undefined') {
+          localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        }
       }
     },
 
+    // Uncomment if needed
     // saveShippingAddress: (state, action) => {
     //   state.shippingAddress = action.payload;
-    //   localStorage.setItem("cart", JSON.stringify(state));
+    //   if (typeof window !== 'undefined') {
+    //     localStorage.setItem("cart", JSON.stringify(state));
+    //   }
     // },
     // savePaymentMethod: (state, action) => {
     //   state.paymentMethod = action.payload;
-    //   localStorage.setItem("cart", JSON.stringify(state));
+    //   if (typeof window !== 'undefined') {
+    //     localStorage.setItem("cart", JSON.stringify(state));
+    //   }
     // },
   },
 });
@@ -70,8 +93,9 @@ export const {
   removeFromCart,
   increaseQuantity,
   decreaseQuantity,
-  //   saveShippingAddress,
-  //   savePaymentMethod,
+  // Uncomment if needed
+  // saveShippingAddress,
+  // savePaymentMethod,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
